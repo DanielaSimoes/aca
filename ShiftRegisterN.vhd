556b436151ac6120dd36178c774a278427ed43c8
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2017-11-18 13:12:29 +0000

    CRC encode done

--- ShiftRegisterN.vhd
+++ ShiftRegisterN.vhd
@@ -4,8 +4,7 @@ use IEEE.STD_LOGIC_1164.all;
 entity ShiftRegisterN is
 		generic(size : positive := 9);
 		port(dataIn : in std_logic_vector(size-1 downto 0);
-			  dataOut: out std_logic_vector(size-2 downto 0));
-			
+			  dataOut: out std_logic_vector(size-2 downto 0));		
 end ShiftRegisterN;
 
 architecture structure of ShiftRegisterN is
