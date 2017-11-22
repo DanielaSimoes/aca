b9be7de073f3c2c244a0f0a7bbbbed684d4d5296
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2017-11-22 01:58:09 +0000

    xor's

---
+++ Encoder2.vhd
@@ -0,0 +1,64 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY Encoder2 IS
+  PORT (a: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        r: OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+END Encoder2;
+
+ARCHITECTURE structure OF Encoder2 IS
+SIGNAL xorOut: STD_LOGIC_VECTOR(8 DOWNTO 0);
+BEGIN
+process(a)
+
+	xorOut: entity work.XorModule(structure)
+				port map(a => a(15 downto 7),
+							b => '111010101',
+							s => xorOut);
+	
+	if xorOut(8) == '0' then			
+		xorOut1: entity work.XorModule(structure)
+					port map(a => xorOut(7 downto 0) & a(6),
+								b => '111010101',
+								s => xorOut);
+	end if;
+	
+	if xorOut(8) == '0' and xorOut(8) == '0' then	
+	xorOut1: entity work.XorModule(structure)
+				port map(a => xorOut(7 downto 0) & a(6),
+							b => '111010101',
+							s => xorOut);
+	end if;
+							
+	xorOut2: entity work.XorModule(structure)
+				port map(a => xorOut(7 downto 0) & a(5),
+							b => '111010101',
+							s => xorOut);
+							
+	xorOut3: entity work.XorModule(structure)
+				port map(a => xorOut(7 downto 0) & a(4),
+							b => '111010101',
+							s => xorOut);
+							
+	xorOut4: entity work.XorModule(structure)
+				port map(a => xorOut(7 downto 0) & a(3),
+							b => '111010101',
+							s => xorOut);
+							
+	xorOut5: entity work.XorModule(structure)
+				port map(a => xorOut(7 downto 0) & a(2),
+							b => '111010101',
+							s => xorOut);
+							
+	xorOut6: entity work.XorModule(structure)
+				port map(a => xorOut(7 downto 0) & a(1),
+							b => '111010101',
+							s => xorOut);
+	
+	xorOut7: entity work.XorModule(structure)
+				port map(a => xorOut(7 downto 0) & a(0),
+							b => '111010101',
+							s => r);
+ 
+end process;
+END structure;
--- XorModule.vhd
+++ XorModule.vhd
@@ -2,12 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity XorModule is 
-	port(a : in std_logic;
-		  b : in std_logic;
-		  s : out std_logic);
-end XorModule;
-
+ port(a : in STD_LOGIC_VECTOR(8 DOWNTO 0);
+ 		  b : in STD_LOGIC_VECTOR(8 DOWNTO 0);
+ 		  s : out STD_LOGIC_VECTOR(8 DOWNTO 0));
+ end XorModule;
+ 
 architecture structure of XorModule is 
 begin
-	s <= a xor b;
+ s <= a xor b;
 end structure;
